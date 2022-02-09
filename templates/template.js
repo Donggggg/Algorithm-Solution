ac531cf2f5d5e54ad32ce2385bf67128b4afcda1
Donggyu Kim <ptsaturn68@daum.net>
2022-02-09 11:00:51 +0900

    템플릿 폴더 생성
    자바스크립트 언어 템플릿 추가
    그로 인한 템플릿 폴더 구조화

---
+++ templates/template.js
@@ -0,0 +1,14 @@
+(function () {
+  let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+  input = input
+    .slice(0, input.length - 1)
+    .map((line) => line.replace('\r', ''));
+    
+    // process the input
+    
+    solution();
+})();
+
+function solution() {
+    // solve the problem
+}
