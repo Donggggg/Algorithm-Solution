e1ea0ec5c1c165fd6e8e9ab9d7e9799c0bc278f0
Donggyu Kim <ptsaturn68@daum.net>
2022-02-24 17:27:04 +0900

    자바스크립트 템플릿 인덴트 수정

--- templates/template.js
+++ templates/template.js
@@ -1,15 +1,13 @@
 function solution() {
-    // solve the problem
+  // solve the problem
 }
 
 (function () {
-  let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+  let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
   input = input
     .slice(0, input.length - 1)
-    .map((line) => line.replace('\r', ''));
-    
-    // process the input
-    solution();
-    
-})();
+    .map((line) => line.replace("\r", ""));
 
+  // process the input
+  solution();
+})();
